21d6a87e53bb920aa5d9a62d8b36a75ee72aea19
carlosab2021 <cabaez2011@gmail.com>
2023-10-23 02:34:58 -0300

    Actualizo main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.wsgi import WSGIMiddleware
 from app.PlayTimeGenre import app as PlayTimeGenre
 from app.sentiment_analysis import app as sentiment_analysis
 from app.UserForGenre import app as UserForGenre
@@ -7,14 +8,15 @@ from app.UsersRecommend import app as UsersRecommend
 
 main_app = FastAPI()
 
-# se monta las aplicaciones
-main_app.mount("/PlayTimeGenre", PlayTimeGenre)
-main_app.mount("/sentiment_analysis", sentiment_analysis)
-main_app.mount("/UserForGenre", UserForGenre)
-main_app.mount("/UsersNotRecommend", UsersNotRecommend)
-main_app.mount("/UsersRecommend", UsersRecommend)
+# Monta la aplicación Flask
+main_app.mount("/PlayTimeGenre", WSGIMiddleware(PlayTimeGenre))
+main_app.mount("/sentiment_analysis", WSGIMiddleware (sentiment_analysis))
+main_app.mount("/UserForGenre", WSGIMiddleware (UserForGenre))
+main_app.mount("/UsersNotRecommend", WSGIMiddleware (UsersNotRecommend))
+main_app.mount("/UsersRecommend", WSGIMiddleware (UsersRecommend))
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(main_app, host="0.0.0.0", port=10000)
+
 
