1506060a6b50a36099db56763c90f728a39bb1f5
carlosab2021 <cabaez2011@gmail.com>
2023-10-23 18:49:44 -0300

    Actualizar main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.openapi.models import APIBase
 from app.PlayTimeGenre import app as PlayTimeGenre
 from app.sentiment_analysis import app as SentimentAnalysis
 from app.UserForGenre import app as UserForGenre
 from app.UsersNotRecommend import app as UsersNotRecommend
 from app.UsersRecommend import app as UsersRecommend
+from fastapi.openapi.models import APIBase
+from fastapi.openapi.models import APIBase
+from fastapi.openapi.models import APIBase
+from fastapi.openapi.models import APIBase
 
 app = FastAPI()
 
@@ -13,6 +18,21 @@ app.mount("/sentiment_analysis", SentimentAnalysis)
 app.mount("/UserForGenre", UserForGenre)
 app.mount("/UsersNotRecommend", UsersNotRecommend)
 app.mount("/UsersRecommend", UsersRecommend)
+
+# Define una función que obtiene la documentación OpenAPI
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="API de Steam Videojuegos",
+        version="1.0",
+        description="App para informacin sobre videos juegos",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 if __name__ == "__main__":
     import uvicorn
