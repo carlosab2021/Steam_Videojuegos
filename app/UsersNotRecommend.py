fa208c2c4f3658813ddb874cd93b778f06aadc7c
carlosab2021 <cabaez2011@gmail.com>
2023-10-23 03:13:33 -0300

    Actualizaciones

--- app/UsersNotRecommend.py
+++ app/UsersNotRecommend.py
@@ -1,19 +1,19 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException
 import pandas as pd
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Carga la base de datos desde el archivo CSV
 data = pd.read_csv('resultado_union_actualizado.csv')
 
 # Ruta para obtener el top 3 de juegos MENOS recomendados por usuarios para un año dado
-@app.route('/UsersNotRecommend/<int:ano>', methods=['GET'])
-def users_not_recommend(ano):
+@app.get('/UsersNotRecommend/{ano}')
+async def users_not_recommend(ano: int):
     # Filtrar los datos para el año especificado
     filtered_data = data[data['fecha_convertida'].str.contains(str(ano))]
 
     if filtered_data.empty:
-        return jsonify({"message": "No se encontraron datos para el año especificado."}, 404)
+        raise HTTPException(status_code=404, detail="No se encontraron datos para el año especificado.")
 
     # Filtrar solo los juegos con recomendaciones negativas y comentarios negativos
     negative_reviews = filtered_data[(filtered_data['recommend'] == False)]
@@ -26,13 +26,10 @@ def users_not_recommend(ano):
     top_not_recommend_games = game_not_recommend_counts.sort_values(by='NotRecommend_Count', ascending=False).head(3)
 
     # Obtener los títulos de los juegos
-    game_titles = data[['item_id', 'title']].drop_duplicates()
+    game_titles = data[['item_id', 'title']].drop_duplicates(subset='item_id')
     top_not_recommend_games = pd.merge(top_not_recommend_games, game_titles, on='item_id')
 
     # Formatear el resultado en el formato requerido
     resultado = [{"Puesto " + str(i + 1): juego['title']} for i, juego in top_not_recommend_games.iterrows()]
 
-    return jsonify(resultado)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return resultado
--- app/sentiment_analysis.py
+++ app/sentiment_analysis.py
@@ -1,19 +1,19 @@
-from flask import Flask, request, jsonify
+from fastapi import FastAPI, HTTPException
 import pandas as pd
 
-app = Flask(__name__)
+app = FastAPI()
 
 # Carga la base de datos con análisis de sentimiento desde el archivo CSV
 data = pd.read_csv('base_de_datos_con_sentimiento.csv')
 
 # Ruta para obtener el análisis de sentimiento según el año de lanzamiento
-@app.route('/sentiment_analysis/<int:ano>', methods=['GET'])
-def sentiment_analysis(ano):
+@app.get('/sentiment_analysis/{ano}')
+async def sentiment_analysis(ano: int):
     # Filtra los datos para el año especificado
     filtered_data = data[data['fecha_convertida'].str.contains(str(ano))]
 
     if filtered_data.empty:
-        return jsonify({"message": "No se encontraron datos para el año especificado."}, 404)
+        raise HTTPException(status_code=404, detail="No se encontraron datos para el año especificado.")
 
     # Realiza el recuento de análisis de sentimiento
     sentiment_counts = filtered_data['sentiment'].value_counts()
@@ -21,7 +21,5 @@ def sentiment_analysis(ano):
     # Convierte el resultado a un diccionario
     result = sentiment_counts.to_dict()
 
-    return jsonify(result)
+    return result
 
-if __name__ == '__main__':
-    app.run(debug=True)
